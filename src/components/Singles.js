a7470417640f39b42cd393d0010ff0dd1222b5dd
Kevin Choi <kevinchoinj@yahoo.com>
2017-10-07 18:33:00 -0400

    zoom effect on thumbnails for singles

--- src/components/Singles.js
+++ src/components/Singles.js
@@ -5,6 +5,8 @@ import img1 from '../images/singles/1.png';
 import img2 from '../images/singles/2.jpg';
 import img3 from '../images/singles/3.jpg';
 
+import Thumbnail from './Thumbnail';
+
 export default class Singles extends React.Component{
 
 
@@ -27,10 +29,6 @@ const singlescontainer={
 	fontWeight:"bold",
 }
 
-const singleimage={
-	width:"100%",
-	border:"1px solid #000"
-}
 
 
 	  return(
@@ -39,15 +37,16 @@ const singleimage={
 				<Row>
 
 				<Col md={4} mdOffset={0} sm={4} smOffset={0} xs={8} xsOffset={2}>
-				<a href="https://fanlink.to/maybe"><img src={img1} style={singleimage}/></a>
+
+				<a href="https://fanlink.to/maybe"><Thumbnail bgimage={img1}/></a>
 				Maybe My Dreams Won't Come True
 				</Col>
 				<Col md={4} mdOffset={0} sm={4} smOffset={0} xs={8} xsOffset={2}>
-				<a href="https://fanlink.to/ridehome"><img src={img2} style={singleimage}/></a>
+				<a href="https://fanlink.to/ridehome"><Thumbnail bgimage={img2}/></a>
 				Ride Home
 				</Col>
 				<Col md={4} mdOffset={0} sm={4} smOffset={0} xs={8} xsOffset={2}>
-				<a href="https://fanlink.to/survive"><img src={img3} style={singleimage}/></a>
+				<a href="https://fanlink.to/survive"><Thumbnail bgimage={img3}/></a>
 				Survive Feat. Rachel</Col>
 
 				</Row>
